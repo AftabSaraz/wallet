7122af814bd6a61d5a78826f84cfb1e696f418e9
AftabSaraz <aftab.saraz@gmail.com>
2019-11-16 15:10:05 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import bcrypt
+
+app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongodb://35.196.137.199:27017")
+db = client.walledDB
+users = db["Users"]
+
+
+def userExist(firstName):
+    if users.find({"firstName": firstName}).count() == 0:
+        return False
+    else:
+        return True
+
+class Index(Resource):
+    def get(self):
+        return "Landed on Index"
+
+class CreateWalletUser(Resource):
+    def post(self):
+        # Get posted data from request
+        data = request.get_json()
+
+        # todo - validations here.
+        # todo - check access key and secret here
+
+        # get data from payload
+        lastName = data["lastName"]
+        firstName = data["firstName"]
+        address = data["address"]
+        email = data["email"]
+        phoneNumber = data["phoneNumber"]
+        language = data["language"]
+        walletOrganizations = data["walletOrganizations"]
+
+        # check if user exists
+        if userExist(firstName):
+            retJson = {
+                "status": 301,
+                "msg": "User Already Exists"
+            }
+            return jsonify(retJson)
+
+        # Insert record
+        users.insert({
+            "lastName": lastName,
+            "firstName": firstName,
+            "address": address,
+            "email": email,
+            "phoneNumber": phoneNumber,
+            "language": language,
+            "walletOrganizations": walletOrganizations
+        })
+
+        # Return successful result
+        retJosn = {
+            "status": "success",
+            "data": {
+                    "walletAccountUser": data
+                   }
+        }
+        return jsonify(retJosn)
+
+
+api.add_resource(Index, '/')
+api.add_resource(CreateWalletUser, '/v1/users')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=8080)
+    # app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
