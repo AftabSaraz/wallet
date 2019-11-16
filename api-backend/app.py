0b9af1f39d499fc0bf9ee980bca883da3ea0979b
AftabSaraz <aftab.saraz@gmail.com>
2019-11-17 00:29:33 +0500

    create_user, fetch one user, and  fetch all users, and json schema validation

--- api-backend/app.py
+++ api-backend/app.py
@@ -1,19 +1,19 @@
 from flask_jsonschema_validator import JSONSchemaValidator
-from flask import Flask, jsonify, request,Response
-from flask_restful import Api, Resource
+from flask import Flask, jsonify, request, Response
+from flask_restful import Resource
 from pymongo import MongoClient
 import bcrypt
 import jsonschema
+from bson.json_util import dumps
+from bson import ObjectId
 
 app = Flask(__name__)
-api = Api(app)
 JSONSchemaValidator(app=app, root="schemas")
-
-app.config["users_schema"] = ""
 
 client = MongoClient("mongodb://35.196.137.199:27017")
 db = client.walledDB
 users = db["Users"]
+
 
 def userExist(firstName):
     if users.find({"firstName": firstName}).count() == 0:
@@ -21,67 +21,94 @@ def userExist(firstName):
     else:
         return True
 
-class Index(Resource):
-    def get(self):
-        return "Landed on Index"
 
-class CreateWalletUser(Resource):
+@app.route('/', methods=['GET'])
+def get():
+    return "Landed on Index"
 
-    @app.errorhandler(jsonschema.ValidationError)
-    def onValidationError(e):
-        return Response("There was a validation error: " + str(e), 400)
 
-    @app.route('/register1', methods=['POST'])
-    @app.validate('users', 'user_schema')
-    def post(self):
-        # Get posted data from request
-        data = request.get_json()
+@app.route('/create_user', methods=['POST'])
+@app.validate('user', 'user_schema')
+def post():
+    # Get posted data from request
+    data = request.get_json()
+    print(data)
+    # todo - validations here.
+    # todo - check access key and secret here
+    # get data from payload
+    lastName = data["lastName"]
+    firstName = data["firstName"]
+    address = data["address"]
+    email = data["email"]
+    phoneNumber = data["phoneNumber"]
+    language = data["language"]
+    walletOrganizations = data["walletOrganizations"]
 
-        # todo - validations here.
-        # todo - check access key and secret here
-
-        # get data from payload
-        lastName = data["lastName"]
-        firstName = data["firstName"]
-        address = data["address"]
-        email = data["email"]
-        phoneNumber = data["phoneNumber"]
-        language = data["language"]
-        walletOrganizations = data["walletOrganizations"]
-
-        # check if user exists
-        if userExist(firstName):
-            retJson = {
-                "status": 301,
-                "msg": "User Already Exists"
-            }
-            return jsonify(retJson)
-
-        # Insert record
-        users.insert({
-            "lastName": lastName,
-            "firstName": firstName,
-            "address": address,
-            "email": email,
-            "phoneNumber": phoneNumber,
-            "language": language,
-            "walletOrganizations": walletOrganizations
-        })
-
-        # Return successful result
-        retJosn = {
-            "status": "success",
-            "data": {
-                    "walletAccountUser": data
-                   }
+    # check if user exists
+    if userExist(firstName):
+        retJson = {
+            "status": 301,
+            "msg": "User Already Exists"
         }
-        return jsonify(retJosn)
+        return jsonify(retJson)
+
+    # Insert record
+    user_id = users.insert({
+        "lastName": lastName,
+        "firstName": firstName,
+        "address": address,
+        "email": email,
+        "phoneNumber": phoneNumber,
+        "language": language,
+        "walletOrganizations": walletOrganizations
+    })
+
+    # Return successful result
+    data.update({"id": str(user_id)})
+    retJosn = {
+        "status": "success",
+        "data": {
+            "walletAccountUser": data
+        }
+    }
+    return jsonify(retJosn)
 
 
-api.add_resource(Index, '/')
-api.add_resource(CreateWalletUser, '/v1/users')
+def fetchUser(user_id):
+    objId = ObjectId(user_id)
+    return users.find({
+        "_id": objId
+    })
+
+
+def fetchAllUsers():
+    return users.find()
+
+
+@app.route('/v1/users', methods=['GET'])
+def retrieveUser():
+
+    user_id = request.args.get('user_id')
+
+    if user_id is None:
+        user_data = fetchAllUsers()
+    else:
+        user_data = fetchUser(user_id)
+
+    # Build successful response
+    retJson = {
+        "status": 200,
+        "data": user_data
+    }
+
+    return dumps(retJson)
+
+
+@app.errorhandler(jsonschema.ValidationError)
+def onValidationError(e):
+    return Response("There was a validation error: " + str(e), 400)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=8080)
+    app.run(host='0.0.0.0', debug=True, port=8088)
     # app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG_MODE)
---
+++ api-backend/users/wallet_user_apis.py
@@ -0,0 +1,43 @@
+from flask_jsonschema_validator import JSONSchemaValidator
+from flask import Flask, jsonify, request,Response
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import bcrypt
+import jsonschema
+
+class Retrieve(Resource):
+    def post(self):
+         # Get posted data from request
+        data = request.get_json()
+
+        # get data
+        username = data["username"]
+        password = data["password"]
+
+        # check if user exists
+        if not userExist(username):
+            retJson = {
+                "status": 301,
+                "msg": "Invalid Username"
+            }
+            return jsonify(retJson)
+
+        # check password
+        correct_pw = verifyUser(username, password)
+        if not correct_pw:
+            retJson = {
+                "status": 302,
+                "msg": "Invalid password"
+            }
+            return jsonify(retJson)
+
+        # get the messages
+        messages = getUserMessages(username)
+
+        # Build successful response
+        retJson = {
+            "status": 200,
+            "obj": messages
+        }
+
+        return jsonify(retJson)
